b488162a2b2a0f80cc4774b250dcb4ccdf1bb567
abtiwary <ab.tiwary@gmail.com>
2023-09-15 10:54:11 +1000

    Install docker
    Also install docker and docker-compose

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ set -e
 home_dir=`realpath ~`
 arg_array=( "$@" )
 
-if [[ ${arg_array[@]} =~ "help" ]]; then
+if [[ ${arg_array[@]} =~ "help" || $# -eq 0 ]]; then
     echo "Install one or more dev environment items."
     echo "The options are:"
-    echo "  all conda neovim rust golang"
+    echo "  all conda neovim rust golang docker"
     exit
 fi
 
@@ -16,7 +16,7 @@ fi
 export TZ=Australia/Sydney
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt update 
+sudo apt update
 sudo apt install -y apt-transport-https ca-certificates
 sudo update-ca-certificates
 sudo apt install -y build-essential tmux screen git clang cmake clang clang-tools libclang-dev clang-tidy cppcheck clang-format valgrind less gdb lldb net-tools traceroute whois curl wget exa
@@ -30,9 +30,8 @@ if [[ ${arg_array[@]} =~ "all" || ${arg_array[@]} =~ "conda" ]]; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     chmod a+rwx Miniconda3-latest-Linux-x86_64.sh
     bash Miniconda3-latest-Linux-x86_64.sh -b -p ${home_dir}/miniconda3
-    bash ${home_dir}/miniconda3/bin/activate
+    #source ${home_dir}/miniconda3/bin/activate
     ${home_dir}/miniconda3/bin/conda install -y -c conda-forge python-kaleido jupyter seaborn numpy matplotlib plotly scipy pymongo toml tomli tomlkit
-    source ${home_dir}/miniconda3/bin/activate
 fi
 
 # neovim
@@ -65,5 +64,20 @@ if [[ ${arg_array[@]} =~ "all" || ${arg_array[@]} =~ "golang" ]]; then
     sudo mv go /opt/google
     #echo "export PATH=\$PATH:/opt/google/go/bin" >> ${home_dir}/.zshrc
     echo "export PATH=\$PATH:/opt/google/go/bin" >> ${home_dir}/.bashrc
-    source "${home_dir}/.zshrc"
+    #source "${home_dir}/.zshrc"
+    source "${home_dir}/.bashrc"
 fi
+
+# docker
+if [[ ${arg_array[@]} =~ "all" || ${arg_array[@]} =~ "docker" ]]; then
+    echo "install docker"
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+    apt-cache policy docker-ce
+    sudo apt install -y docker-ce docker-compose
+    sudo systemctl status docker
+    sudo usermod -aG docker ${USER}
+fi
+
